1ef57a377787023c998f5d39088efebcf1a264bd
Nightinggale <Nightinggale.coding@gmail.com>
2013-09-29 15:11:47 +0200

    Perl: added makegroups and newyield commands to write perl output directly into the source

--- scripts/yield.pl
+++ scripts/yield.pl
@@ -11,6 +11,10 @@
 # python: outputs the code for CyEnumsInterface.cpp
 #
 # checkXML: outputs the code for BaseCheckYieldGroup::checkXML in Yields_(mod).cpp
+#
+# makegroups: generate all groups and write them directly into the header file
+#
+# newyield: calls python and checkXML and writes the result directly into the source
 
 use strict;
 use warnings;
@@ -29,12 +33,21 @@ foreach (@ARGV)
 		} else {
 			$group = 'Check_' . $_;
 		}
-	} elsif (substr($_,0, 6) eq 'python') {
+	} elsif ($_ eq 'python') {
 		$group = 'python';
-	} elsif (substr($_,0, 8) eq 'checkXML') {
+	} elsif ($_ eq 'checkXML') {
 		$group = 'checkXML';
+	} elsif ($_ eq 'makegroups') {
+		$group = 'makegroups';
+	} elsif ($_ eq 'newyield') {
+		$group = 'newyield';
 	} 
 }
+
+my $HEADER_FILE = "../DLL_Sources/Yields_" . $target . ".h";
+my $SOURCE_FILE = "../DLL_Sources/Yields_" . $target . ".cpp";
+
+my $to_file = 0;
 
 ##
 ## generate a hash and an array with all yields
@@ -48,6 +61,14 @@ my %yield_hash = ();
 my %yield_convertion = ();
 
 while (<FILE>) {
+	my $comment_index = index($_, '//');
+	my $yield_index = index($_, 'YIELD');
+	
+	if ($comment_index != -1 and $comment_index < $yield_index)
+	{
+		next;
+	}
+	
 	if ($status == 0) {
 		if (index($_, 'NO_YIELD = -1') != -1) {
 			$status = 1;
@@ -73,7 +94,10 @@ while (<FILE>) {
 			$right = substr($right, index($right, 'Y'));
 			$right = substr($right,0,index($right, ',') );
 			
-			$yield_convertion{$right} = $left;
+			if ($left ne "YIELD_FROM_ANIMALS")
+			{
+				$yield_convertion{$right} = $left;
+			}
 		}
 	}
 }
@@ -82,6 +106,13 @@ close FILE;
 
 # at this point we can look up yield_names by index in @yield_array and yield_index by name in %yield_hash
 
+sub writeoutput {
+	if ($to_file == 0) {
+		print $_[0];
+	} else {
+		print WRITE_FILE $_[0];
+	}
+}
 
 
 sub GetOptimizedgroup{
@@ -119,7 +150,7 @@ sub GetOptimizedgroup{
 	close FILE;
 	
 	
-	print "\treturn ";
+	writeoutput("\treturn ");
 	
 	my $num_yields = scalar(@yield_array);
 	
@@ -139,14 +170,14 @@ sub GetOptimizedgroup{
 			if ($last != -1)
 			{
 				if ($empty == 0) {
-					print " || "
+					writeoutput(" || ");
 				}
 				$empty = 0;
 				if (($num_yields - $first) > ($num_yields + ($last - $num_yields)))
 				{
-					print "(eYield <= " . $yield_array[$last] . " && eYield >= " . $yield_array[$first] . ")";
+					writeoutput("(eYield <= " . $yield_array[$last] . " && eYield >= " . $yield_array[$first] . ")");
 				} else {
-					print "(eYield >= " . $yield_array[$first] . " && eYield <= " . $yield_array[$last] . ")";
+					writeoutput("(eYield >= " . $yield_array[$first] . " && eYield <= " . $yield_array[$last] . ")");
 				}
 			}
 			$first = -1;
@@ -169,10 +200,10 @@ sub GetOptimizedgroup{
 		} elsif ($first != -1 and $_ == -1) {
 			if ($last == -1) {
 				if ($empty == 0) {
-					print " || "
+					writeoutput(" || ");
 				}
 				$empty = 0;
-				print "(eYield == " . $yield_array[$first] . ")";
+				writeoutput("(eYield == " . $yield_array[$first] . ")");
 			}
 			$first = -1;
 			$last = -1;
@@ -180,30 +211,152 @@ sub GetOptimizedgroup{
 	}
 	
 	if ($empty == 1) {
-		print "false";
+		writeoutput("false");
 	}
-	print ";\n";
+	writeoutput(";\n");
 }
 
 sub checkXML {
-	print "Fill in Yields_" . $target . ".cpp\n";
-	print "in function BaseCheckYieldGroup::checkXML\n";
+	if ($to_file == 0) {
+		print "Fill in Yields_" . $target . ".cpp\n";
+		print "in function BaseCheckYieldGroup::checkXML\n";
+	} else {
+		print "writing XML checking code to " . $SOURCE_FILE . "\n";
+	}
 	for (@yield_array) {
-		print "\tcheckSingleXMLType(" . $_ . ",";
+		writeoutput("\tcheckSingleXMLType(" . $_ . ",");
 		for (my $count = 26; $count >= length $_; $count--) {
-			print " ";
+			writeoutput(" ");
 		}
-		print "\"" . $_ . "\");\n";
+		writeoutput("\"" . $_ . "\");\n");
 	}
 }
 
 sub createPythonAccess {
-	print "Fill in CyEnumsInterface.cpp\n";
-	print "python::enum_<YieldTypes>(\"YieldTypes\")\n";
-	print "under " . $target . "\n";
-	for (@yield_array) {
-		print "\t\t.value(\"" . $yield_convertion{$_} . "\", " . $_ . ")\n";
+	if ($to_file == 0) {
+		print "Fill in CyEnumsInterface.cpp\n";
+		print "python::enum_<YieldTypes>(\"YieldTypes\")\n";
+		print "under " . $target . "\n";
+	} else {
+		print "writing python access code to CyEnumsInterface.cpp\n";
 	}
+	for (@yield_array) {
+		my $new_yield = $yield_convertion{$_};
+		writeoutput("\t\t.value(\"" . $new_yield . "\", ");
+		for (my $count = 26; $count >= length $new_yield; $count--) {
+			writeoutput(" ");
+		}
+		writeoutput($_ . ")\n");
+	}
+}
+
+sub writeYieldXMLAccess {
+	open (FILE, "< " . $SOURCE_FILE) or die "Can't open file " . $SOURCE_FILE . "\n" . $!;
+	my @lines = <FILE>;
+	close FILE;
+	
+	open (WRITE_FILE, "> " . $SOURCE_FILE) or die "Can't open file " . $SOURCE_FILE . "\n" . $!;
+	
+	$to_file = 1;
+	
+	my $found = 0;
+	foreach (@lines)
+	{
+		if ($found == 0 and index($_, "checkSingleXMLType") != -1)
+		{
+			$found = 1;
+			checkXML();
+		} elsif ($found == 1)
+		{
+			if (substr($_,0,1) eq "}")
+			{
+				$found = 2;
+				print WRITE_FILE $_;
+			}
+		} else {
+			print WRITE_FILE $_;
+		}
+	}
+	
+	close WRITE_FILE;
+}
+
+sub writeYieldPythonAccess {
+	my $source = "../DLL_Sources/CyEnumsInterface.cpp";
+	open (FILE, "< " . $source) or die "Can't open file " . $source . "\n" . $!;
+	my @lines = <FILE>;
+	close FILE;
+	
+	open (WRITE_FILE, "> " . $source) or die "Can't open file " . $source . "\n" . $!;
+	
+	$to_file = 1;
+	
+	my $define = "";
+	
+	if ($target eq 'Medieval_Tech')
+	{
+		$define = "MEDIEVAL_TECH";
+	} elsif ($target eq 'Colonization_2071')
+	{
+		$define = "COLONIZATION_2071";
+	} else {
+		die "unknown target!\n";
+	}
+	
+	my $status = 0;
+	foreach (@lines)
+	{
+
+		if ($status == 0 and index($_, 'YieldTypes') != -1)
+		{
+			$status = 1;
+			print WRITE_FILE $_;
+		} elsif ($status == 1 and substr($_, 0, 1) eq "#" and index($_, $define) != -1)
+		{
+			$status = 2;
+			print WRITE_FILE $_;
+			createPythonAccess();
+		} elsif ($status == 2)
+		{
+			if (substr($_, 0, 1) eq "#") {
+				$status = 3;
+				print WRITE_FILE $_;
+			}
+		} else {
+			print WRITE_FILE $_;
+		}
+	}
+	
+	close WRITE_FILE;
+}
+
+sub makeAllGroups {
+	open (FILE, "< " . $HEADER_FILE) or die "Can't open file " . $HEADER_FILE . "\n" . $!;
+	my @lines = <FILE>;
+	close FILE;
+	
+	open (WRITE_FILE, "> " . $HEADER_FILE) or die "Can't open file " . $HEADER_FILE . "\n" . $!;
+	
+	$to_file = 1;
+	my $function = "";
+	
+	foreach (@lines)
+	{
+		if (substr($_,0,29) eq "static inline bool YieldGroup")
+		{
+			$function = substr($_, index($_, "YieldGroup"));
+			$function = substr($function, 0, index($function, "("));
+		}
+		
+		if ($function ne "" and index($_, "return") != -1)
+		{
+			GetOptimizedgroup("Check_" . $function);
+			$function = "";
+		} else {
+			print WRITE_FILE $_;
+		}
+	}
+	close WRITE_FILE;
 }
 
 
@@ -215,4 +368,9 @@ if (substr($group,0, 16) eq 'Check_YieldGroup') {
 	createPythonAccess();
 } elsif ($group eq 'checkXML') {
 	checkXML();
+} elsif ($group eq 'makegroups') {
+	makeAllGroups();
+} elsif ($group eq 'newyield') {
+	writeYieldXMLAccess();
+	writeYieldPythonAccess();
 }
